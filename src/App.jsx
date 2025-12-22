ca320e38a9c2562411c07197bc67fd0259e87ce3
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-12-23 00:28:27 +0100

    dialer buttons done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Dialer from './components/Dialer'
+
+function App() {
+
+  return (
+    <>
+    <h1>Phone dialer using DTMF tones</h1>
+
+    <Dialer />
+    </>
+  )
+}
+
+export default App
