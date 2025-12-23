7563fc5428300b939b9dcf5a51f082eb192b84c5
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-12-23 01:16:43 +0100

    Added DTMF tones to buttons

--- src/components/Dialer.jsx
+++ src/components/Dialer.jsx
@@ -1,23 +1,71 @@
-import React from 'react'
+import React, { useRef } from 'react'
+
+    const DTMF_FREQUENCIES = {
+        '1': [697, 1209],
+        '2': [697, 1336],
+        '3': [697, 1477],
+        '4': [770, 1209],
+        '5': [770, 1336],
+        '6': [770, 1477],
+        '7': [852, 1209],
+        '8': [852, 1336],
+        '9': [852, 1477],
+        '*': [941, 1209],
+        '0': [941, 1336],
+        '#': [941, 1477],
+    }
+
+   const keys = ['1','2','3','4','5','6','7','8','9','*','0','#']
 
 export default function Dialer() {
+    const audioContextRef = useRef(null)
+
+    const playDTMF = (key, duration = 150) => {
+        if (!DTMF_FREQUENCIES[key]) return
+
+        if (!audioContextRef.current) {
+            audioContextRef.current =
+                new (window.AudioContext || window.webkitAudioContext)()
+        }
+
+        const audioContext = audioContextRef.current
+        const [low, high] = DTMF_FREQUENCIES[key]
+
+        const gainNode = audioContext.createGain()
+        gainNode.gain.value = 0.1
+        gainNode.connect(audioContext.destination)
+
+        const osc1 = audioContext.createOscillator()
+        const osc2 = audioContext.createOscillator()
+
+        osc1.frequency.value = low
+        osc2.frequency.value = high
+
+        osc1.connect(gainNode)
+        osc2.connect(gainNode)
+
+        osc1.start()
+        osc2.start()
+
+        setTimeout(() => {
+            osc1.stop()
+            osc2.stop()
+        }, duration)
+    }
+
+
+
   return (
     <div className='dialer'>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-
-            <button>*</button>
-            <button>0</button>
-            <button>#</button>
+        {keys.map((key) => (
+            <button
+                key={key}
+                onClick={() => playDTMF(key)}
+                aria-label={`Dial ${key}`}
+            >
+                {key}
+            </button>
+        ))}
     </div>
   )
 }
